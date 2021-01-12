ff59914fafdbbad38e7129b51ae6fa63c30ca475
Hoon <thy0722@supertree.co>
2021-01-12 20:19:24 +0900

    add tcp listen

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	runSocket()
+}
+
+func runSocket() {
+	url := "127.0.0.1:9999"
+	fmt.Println("listen on: ", url)
+
+	ln, err := net.Listen("tcp", url)
+	if err != nil {
+		log.Println("err: ", err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("err: ", err)
+		}
+
+		tcpConn := conn.(*net.TCPConn)
+		go handleConnection(tcpConn)
+	}
+}
+
+func handleConnection(tcpConn *net.TCPConn) {
+	b := make([]byte, 256)
+
+	_, err := tcpConn.Read(b)
+	if err != nil {
+		log.Println("err: ", err)
+	} else {
+		log.Println("msg: ", string(b))
+	}
 }
